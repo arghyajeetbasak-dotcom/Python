e34059bcdf0d0b61f4ea55b12d8ba89da8cbe5a2
Arghyajeet Basak <arghyajeetbasak@gmail.com>
2026-08-19 19:29:40 +0530

    Edited recursion file

--- DSA/recursion.py
+++ DSA/recursion.py
@@ -14,3 +14,31 @@ def display(n):
     display(n-1)
     print(n)
 display(5)
+
+def recurse(i,n):
+    if(i>n):
+        return
+    print(i)
+    recurse(i+1,n)
+recurse(1,12)
+
+def tailrecurse(n):
+    if(n==0):
+        return
+    tailrecurse(n-1)
+    print(n)
+tailrecurse(9)
+
+def rev(n):
+    if(n==0):
+        return
+    print(n)
+    rev(n-1)
+rev(9)
+
+def tailrev(i,n):
+    if(i>n):
+        return
+    tailrev(i+1,n)
+    print(i)
+tailrev(1,7)
